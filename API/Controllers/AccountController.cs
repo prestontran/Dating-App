8eb4ee75dc6bb914422f7faf0d91f3ed25200ee9
Preston Tran <81770985+prestontran@users.noreply.github.com>
2023-08-28 18:42:00 -0500

    Stash checked in

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,34 +1,16 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
-<<<<<<< Updated upstream
-using API.Entities;
-using Microsoft.AspNetCore.Mvc;
-using SQLitePCL;
-=======
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
->>>>>>> Stashed changes
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-<<<<<<< Updated upstream
-       private readonly DataContext _context;
-       
-        public AccountController(DataContext context)
-        {
-            _context = context;
-        }
-
-    [HttpPost("register")] // POST: api/account/register
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
-        {
-=======
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
        
@@ -43,27 +25,18 @@ namespace API.Controllers
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
->>>>>>> Stashed changes
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-<<<<<<< Updated upstream
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-=======
                 UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
->>>>>>> Stashed changes
                 PasswordSalt = hmac.Key
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-<<<<<<< Updated upstream
-            return user;
-=======
             return new UserDto
             {
                 Username = user.UserName,
@@ -97,7 +70,6 @@ namespace API.Controllers
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
->>>>>>> Stashed changes
         }
     }
 }

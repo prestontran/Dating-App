ebc269220bcc15b3f51bfb8dec2b4f3973b0f7da
Preston Tran <81770985+prestontran@users.noreply.github.com>
2023-08-28 18:16:54 -0500

    8/28/2023

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,14 +1,23 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+<<<<<<< Updated upstream
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using SQLitePCL;
+=======
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+>>>>>>> Stashed changes
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
+<<<<<<< Updated upstream
        private readonly DataContext _context;
        
         public AccountController(DataContext context)
@@ -19,19 +28,76 @@ namespace API.Controllers
     [HttpPost("register")] // POST: api/account/register
     public async Task<ActionResult<AppUser>> Register(string username, string password)
         {
+=======
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+       
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+            _context = context;
+        }
+
+        [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+>>>>>>> Stashed changes
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
+<<<<<<< Updated upstream
                 UserName = username,
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+=======
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+>>>>>>> Stashed changes
                 PasswordSalt = hmac.Key
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
+<<<<<<< Updated upstream
             return user;
+=======
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+>>>>>>> Stashed changes
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Dating-App
+1. This Dating-App project has 2 parts: API folder (C# backend) and client folder (Angular frontend)
+   
+2. API folder is the backend code. To run backend, we need to type in "dotnet watch --no-hot-reload" in the terminal
+    Helpful backend C# terminal commands:
+    dotnet tool install --global dotnet-ef --version 7.0.5 (from nuget dotnet-ef website)
+    dotnet tool list -g
+    dotnet tool -h
+    dotnet ef database -h
+    dotnet ef migrations add UsernameRequired (add new required field for database)
+    dotnet ef migrations remove (remove latest change in database)
+
+    command pallet: .net:generate assets for build and debug: creates launch.json file
+    
+3. client folder is the frontend code. To run frontend, we need to type in "ng serve"
+    If possible, install SSL signed certificate for localhost for development. Use Chocolatey for mkcert
+
+    To debug frontend, there are 2 ways: 1. run backend: dotnet run and run .NET Core Launch (web)
+                                         2. run backend: dotnet watch --no-hot-reload and run .NET Core Attach and choose .exe file in the command pallet
